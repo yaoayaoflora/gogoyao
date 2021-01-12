cecaa0e6ac0b5f944fd6639ba4a9c5277b93efc5
Yao Feng <yao.feng531@hotmail.com>
2021-01-12 09:24:04 +0800

    20210112

--- DataStructuresAlgorithms/grokking-the-coding-interview-patterns-for-coding-questions/1_Sliding_Window/1_6.py
+++ DataStructuresAlgorithms/grokking-the-coding-interview-patterns-for-coding-questions/1_Sliding_Window/1_6.py
@@ -4,6 +4,7 @@
 
 def length_of_longest_substring(str1, k):
     startIndex = 0
+<<<<<<< Updated upstream
     maxLength = 0
     maxRepeatCharCount = 0
     charsFrequency = {}
@@ -19,12 +20,32 @@ def length_of_longest_substring(str1, k):
             leftChar = str1[startIndex]
             startIndex += 1
             charsFrequency[leftChar] -= 1
+=======
+    maxRepeatLetterCount = 0
+    maxLength = 0
+    charFrequency = {}
+
+    for endIndex in range(len(str1)):
+        rightChar = str1[endIndex]
+        if rightChar not in charFrequency:
+            charFrequency[rightChar] = 0
+        charFrequency[rightChar] += 1
+        maxRepeatLetterCount = max(maxRepeatLetterCount, charFrequency[rightChar])
+
+        if endIndex - startIndex + 1 - maxRepeatLetterCount > k:
+            leftChar = str1[startIndex]
+            startIndex += 1
+            charFrequency[leftChar] -= 1
+>>>>>>> Stashed changes
         
         maxLength = max(maxLength, endIndex - startIndex + 1)
 
+<<<<<<< Updated upstream
     return maxLength
     
 
+=======
+>>>>>>> Stashed changes
 def main():
     print(length_of_longest_substring("aabccbb", 2))
     print(length_of_longest_substring("abbcb", 1))
