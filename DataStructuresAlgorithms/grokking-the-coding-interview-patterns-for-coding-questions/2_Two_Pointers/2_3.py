2adf804b252934565432a96ce5c8ef7526388509
yaoayao <yao.feng531@hotmail.com>
2021-01-13 10:44:05 +0800

    20210113

--- DataStructuresAlgorithms/grokking-the-coding-interview-patterns-for-coding-questions/2_Two_Pointers/2_3.py
+++ DataStructuresAlgorithms/grokking-the-coding-interview-patterns-for-coding-questions/2_Two_Pointers/2_3.py
@@ -1,30 +1,8 @@
-# Given a sorted array, create a new array containing squares of all the number of the input
-# array in the sorted order.
+# Given a sorted array, create a new array containing squares of all the number of the input array in the sorted order.
 
 def make_squares(arr):
     squares = []
-    nextElement = 0
-    left = 0
-    right = len(arr) - 1
-
-    while left < right:
-        leftSquare = arr[left] * arr[left]
-        rightSquare = arr[right] * arr[right]
-
-        if leftSquare < rightSquare:
-            squares.append(leftSquare)
-            left += 1
-        elif leftSquare > rightSquare:
-            squares.append(rightSquare)
-            right -= 1
-        else:
-            squares.append(leftSquare)
-            left += 1
-            squares.append(rightSquare)
-            right -= 1
-        
-
-
+    
 
 
 def main():
@@ -33,4 +11,3 @@ def main():
 
 
 main()
-        
