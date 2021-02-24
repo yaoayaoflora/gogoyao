f9cfc4608eeee92ca1547408f93954c524a241cb
yaoayao <yao.feng531@hotmail.com>
2021-02-24 17:47:32 +0800

    20210224

--- DataStructuresAlgorithms/grokking-the-coding-interview-patterns-for-coding-questions/11_Modified_Binary_Search/11_9.py
+++
@@ -1,8 +0,0 @@
-# Given an array of numbers which is sorted in ascending order and also rotated by some arbitrary number, 
-# find if a given ‘key’ is present in it.
-# 
-# Write a function to return the index of the ‘key’ in the rotated array. If the ‘key’ is not present, return -1. 
-# You can assume that the given array does not have any duplicates.
- 
-
- 
